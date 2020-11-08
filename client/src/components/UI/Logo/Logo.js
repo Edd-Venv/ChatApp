5ee52a61dbab3378b3c87e994522d89c023cbb9f
Edd-Venv <eushibantu@gmail.com>
2020-11-08 12:49:52 +0100

    Logo Creation

--- client/src/components/UI/Logo/Logo.js
+++ client/src/components/UI/Logo/Logo.js
@@ -1,17 +1,15 @@
 import React from "react";
 import PropTypes from "proptypes";
-import classes from "./Logo.modules.css";
+import classes from "./Logo.module.css";
 
 function Logo(props) {
   const { image } = props;
   return (
-    <div>
-      <img
-        className={classes.LogoImg}
-        src={`https://userspictures.s3.us-east-2.amazonaws.com/${image}`}
-        alt="Logo"
-      />
-    </div>
+    <img
+      className={classes.LogoImg}
+      src={`https://userspictures.s3.us-east-2.amazonaws.com/${image}`}
+      alt="Logo"
+    />
   );
 }
 
