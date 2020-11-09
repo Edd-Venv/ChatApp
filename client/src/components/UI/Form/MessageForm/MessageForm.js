20edbd0337264467c96beb97841adccc03eb8d15
Edd-Venv <eushibantu@gmail.com>
2020-11-09 11:11:40 +0100

    MessageForm Update

--- client/src/components/UI/Form/MessageForm/MessageForm.js
+++ client/src/components/UI/Form/MessageForm/MessageForm.js
@@ -1,14 +1,20 @@
 import React from "react";
 import PropTypes from "proptypes";
+import classes from "./MessageForm.module.css";
 
 function MessageForm(props) {
-  const { onChange, onSubmit, value } = props;
-
+  const { onChange, onSubmit, value, texts } = props;
+  console.log("messageForm", texts);
   return (
-    <div style={{ display: "flex" }}>
-      <ul id="ul" />
-      <form onSubmit={onSubmit} autoComplete="off">
-        <input onChange={onChange} name="input" value={value} />
+    <div className={classes.Container}>
+      <ul id="ul" className={classes.UL} />
+      <form onSubmit={onSubmit} autoComplete="off" className={classes.Form}>
+        <input
+          onChange={onChange}
+          name="input"
+          value={value}
+          className={classes.Input}
+        />
         <button type="submit">Send</button>
       </form>
     </div>
@@ -19,6 +25,7 @@ MessageForm.propTypes = {
   onChange: PropTypes.func,
   onSubmit: PropTypes.func,
   value: PropTypes.string,
+  texts: PropTypes.array,
 };
 
 export default MessageForm;
