25c8ca0eba2bd59d5618dc80e430dd0ccc4d2bf9
Edd-Venv <eushibantu@gmail.com>
2020-11-20 21:06:31 +0100

    Added Typing FeedBack Feature

--- client/src/components/Pages/Message/Message.js
+++ client/src/components/Pages/Message/Message.js
@@ -12,6 +12,7 @@ import { BaseUrl, socket } from "../../../App";
 import MessageForm from "../../UI/Form/MessageForm/MessageForm";
 import messageHandler, { getDate } from "../Utils/Utils";
 import classes from "./Message.module.css";
+import messageFormClasses from "../../UI/Form/MessageForm/MessageForm.module.css";
 import Contacts from "./Contacts/Contacts";
 
 function Message() {
@@ -19,7 +20,14 @@ function Message() {
   const [socketState, socketDispath] = useContext(SocketContext);
   const [message, setMessage] = useState("");
   const [texts, setTexts] = useState([]);
-  const { userId, userName, selectedContact, authenticated, jwt } = state;
+  const {
+    userId,
+    userName,
+    selectedContact,
+    authenticated,
+    jwt,
+    userImage,
+  } = state;
   const id_uid = useMemo(() => selectedContact.id_uid, [selectedContact]);
 
   useEffect(() => {
@@ -37,22 +45,66 @@ function Message() {
         .then((res) => res.json())
         .then((result) => {
           if (result.message) console.log("err", result.message);
-          else setTexts(result.texts);
+          else {
+            setTexts(result.texts);
+          }
         });
 
     socket.on("received-message", (dta) => {
-      messageHandler(dta.message, dta.from.userName, "recieved", dta.timeStamp);
+      const feedback = document.getElementById("feedback");
+      const messagesFeed = document.getElementById("ul");
+      if (feedback) {
+        feedback.innerHTML = "";
+        feedback.style.display = "none";
+        messagesFeed.style.paddingBottom = "60px";
+      }
+      messageHandler(
+        dta.message,
+        dta.from.userName,
+        "recieved",
+        dta.timeStamp,
+        selectedContact.person_image
+      );
     });
 
     socket.on("sent-message", (info) => {
-      messageHandler(info.message, info.from.userName, "sent", info.timeStamp);
+      messageHandler(
+        info.message,
+        info.from.userName,
+        "sent",
+        info.timeStamp,
+        userImage
+      );
+    });
+
+    socket.on("typing", (info) => {
+      const feedback = document.getElementById("feedback");
+      const messagesFeed = document.getElementById("ul");
+
+      if (feedback) {
+        const p = document.createElement("p");
+        const em = document.createElement("em");
+        em.innerHTML = `${info.from} is typing...`;
+        p.innerHTML = em.innerHTML;
+        feedback.innerHTML = p.innerHTML;
+        feedback.className = messageFormClasses.Typing;
+        feedback.style.display = "flex";
+        feedback.scrollIntoView({ smooth: true });
+        messagesFeed.style.paddingBottom = 0;
+      }
     });
 
     return () => {
       socket.off("received-message");
       socket.off("sent-message");
+      socket.off("typing");
     };
   }, [selectedContact]);
+
+  const onKeyPress = useCallback((event) => {
+    const data = { to: id_uid, from: userName };
+    socket.emit("typing", data);
+  });
 
   const handleSubmit = useCallback((event) => {
     event.preventDefault();
@@ -77,10 +129,12 @@ function Message() {
     <div className={classes.Container}>
       <Contacts />
       <MessageForm
+        onKeyPress={onKeyPress}
         texts={texts}
         onChange={handleChange}
         onSubmit={handleSubmit}
         value={message}
+        state={state}
       />
     </div>
   );
